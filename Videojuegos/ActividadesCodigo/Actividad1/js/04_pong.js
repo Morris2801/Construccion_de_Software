67b1062bc7a314975a47200b6ac0b7649b2020c0
Mauricio Emilio Monroy González <mauriciomonroyg@hotmail.com>
2025-02-27 13:21:15 -0600

    Modified PONG game so that collisions are a thing, the paddles respect boundaries, the ball bounces back, a score tracking was added, and modified dimensions of the canvas a bit

--- Videojuegos/ActividadesCodigo/Actividad1/js/04_pong.js
+++ Videojuegos/ActividadesCodigo/Actividad1/js/04_pong.js
@@ -5,20 +5,33 @@ Implementation of the game of Pong
 "use strict";
 
 // Global variables
-const canvasWidth = 800;
-const canvasHeight = 600;
+const canvasWidth = 900;
+const canvasHeight = 700;
 let oldTime;
-const paddleVelocity = 2;
+const paddleVelocity = 1.25;
+let leftScore = 0; 
+let rightScore = 0;
 
 // Context of the Canvas
 let ctx;
+class Box extends GameObject {
+    constructor(position,width,height,color,type){
+        super(position,width,height,color, "box");
+    }
+}
+
 class Ball extends GameObject {
     constructor(position,width,height,color,type){
         super(position,width,height,color, "ball");
-        this.velocity = new Vec(0.01,0.01);
+        this.velocity = new Vec(0.25,0.25);
     }
     update(deltaTime){
         this.position = this.position.plus(this.velocity.times(deltaTime));
+    }
+    initVelocity(){
+        box.position = new Vec(canvasWidth/2, canvasHeight/2);
+        let angle = Math.random()*(Math.PI/2) - (Math.PI/4);
+        this.velocity = new Vec(Math.cos(angle), Math.sin(angle));
     }
 }
 class Paddle extends GameObject {
@@ -28,12 +41,22 @@ class Paddle extends GameObject {
     }
     update(deltaTime){
         this.position = this.position.plus(this.velocity.times(deltaTime));
+        if(this.position.y <0){
+            this.position.y = 0; 
+        }
+        else if(this.position.y + this.height > canvasHeight){
+            this.position.y = canvasHeight - this.height
+        }
     }
 }
 
 const box = new Ball(new Vec(canvasWidth/2, canvasHeight/2), 20, 20, "white");
 const leftPaddle = new Paddle(new Vec(20, canvasHeight/2), 20,100, "rgb(255, 89, 89)");
 const rightPaddle = new Paddle(new Vec(canvasWidth-40, canvasHeight/2), 20,100, "rgb(255, 89, 89)");
+const upMarg = new Box(new Vec(0,0), canvasWidth, 20, "grey");
+const downMarg = new Box(new Vec(0,canvasHeight-20), canvasWidth, 20, "grey");
+const leftMarg = new Box(new Vec(0,0), 20, canvasHeight, "black");
+const rightMarg = new Box(new Vec(canvasWidth-20,0), 20, canvasHeight, "black");
 
 function main() {
     // Get a reference to the object with id 'canvas' in the page
@@ -89,18 +112,31 @@ function drawScene(newTime) {
     box.draw(ctx);
     leftPaddle.draw(ctx);
     rightPaddle.draw(ctx);
+    upMarg.draw(ctx);
+    downMarg.draw(ctx);
+    leftMarg.draw(ctx);
+    rightMarg.draw(ctx);
     box.update(deltaTime);
     leftPaddle.update(deltaTime);
     rightPaddle.update(deltaTime);
-    // Update the properties of the object
-    // box.x += box.speed * box.direction;
-    // if (box.position.x > canvasWidth - box.width) {
-    //     box.direction = -1;
-    //     box.speed += 0.1;
-    // } 
-    // else if (box.x < 0) {
-    //     box.direction = 1;
-    // }
+
+    if(boxOverlap(box, leftPaddle) || boxOverlap(box, rightPaddle)){
+        box.velocity.x *= -1; 
+        box.velocity = box.velocity.times(1.1);
+    }
+    if(boxOverlap(box, upMarg) || boxOverlap(box, downMarg)){
+        box.velocity.y *= -1;
+        box.velocity = box.velocity.times(1.1);
+    }
+    if(boxOverlap(box, leftMarg)){
+        rightScore++;
+        box.initVelocity();
+    }
+    if(boxOverlap(box, rightMarg)){
+        leftScore++;
+        box.initVelocity();
+    }  
+    
     oldTime = newTime; 
     requestAnimationFrame(drawScene);
 }
--- Videojuegos/ActividadesCodigo/Actividad1/js/game_classes.js
+++ Videojuegos/ActividadesCodigo/Actividad1/js/game_classes.js
@@ -41,3 +41,10 @@ class GameObject{
 
     }
 }
+
+function boxOverlap(obj1, obj2){
+    return obj1.position.x + obj1.width > obj2.position.x && 
+    obj1.position.x < obj2.position.x + obj2.width &&
+    obj1.position.y + obj1.height > obj2.position.y &&
+    obj1.position.y < obj2.position.y + obj2.height;
+}
